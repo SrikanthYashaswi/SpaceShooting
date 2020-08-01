2bbb3ccacae987939126b422fde95e5a8bc20eed
srikanth <srikanth@finzy.com>
2020-08-01 17:14:15 +0530

    Srikanth | Refactored ship class, Extended Ship with Mass to introduce health capability.

--- src/main/java/org/spaceimpact/models/base/Ship.java
+++ src/main/java/org/spaceimpact/models/base/Ship.java
@@ -1,12 +1,10 @@
 package org.spaceimpact.models.base;
 
-public abstract class Ship extends Sprite {
+public abstract class Ship extends Mass {
     private Direction cannonDirection;
-    private int availableHealth;
 
-    public Ship(int x, int y, String c, int availableHealth) {
-        super(x, y, c);
-        this.availableHealth = availableHealth;
+    public Ship(int x, int y, String c, int health) {
+        super(x, y, c, health);
     }
 
     public Direction getCannonDirection() {
@@ -15,16 +13,5 @@ public abstract class Ship extends Sprite {
 
     public void setCannonDirection(Direction cannonDirection) {
         this.cannonDirection = cannonDirection;
-    }
-
-    public int getAvailableHealth() {
-        return this.availableHealth;
-    }
-
-    public void impact(int power) {
-        this.availableHealth -= power;
-        if (this.availableHealth <= 0) {
-            this.markInactive();
-        }
     }
 }
